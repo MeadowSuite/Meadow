35b89e1a05d577a80b01337d5b1993855c583129
Matt Little <zone117x@gmail.com>
2018-09-21 11:46:23 -0700

    Remove some unused code

--- Meadow.Contract/BaseContract.cs
+++ Meadow.Contract/BaseContract.cs
@@ -86,11 +86,4 @@ namespace Meadow.Contract
             JsonRpcClient = rpcClient;
         }
     }
-
-    public class ContractConstructParams
-    {
-        public Uri Server { get; set; }
-
-        public Address DefaultFromAccount { get; set; }
-    }
 }
