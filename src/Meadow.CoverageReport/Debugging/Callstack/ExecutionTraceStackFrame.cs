10f58bd038c664d9fbb3bd81eb0ff6257af76b28
David Pokora <dpokora@gmail.com>
2018-09-29 05:54:00 -0400

    Rewrote callstack logic, affecting both exception handling and debugger stack frames: * Better heuristics for resolving current source line in tough situations. * Better behavior when encountering errors/out-of-function scope calls. * Inherently addresses a problem where VSCode debugger would have no callstack if you were outside of a function scope, and could lose focus on the thread, requiring thread selection before taking further steps on the intended thread. * Addresses a problem where debugger would not know how to construct stack frames in certain situations, throwing an error previously.

---
+++ src/Meadow.CoverageReport/Debugging/Callstack/ExecutionTraceStackFrame.cs
@@ -0,0 +1,79 @@
+﻿using Meadow.CoverageReport.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meadow.CoverageReport.Debugging.Callstack
+{
+    public struct ExecutionTraceStackFrame
+    {
+        #region Properties
+        /// <summary>
+        /// Indicates the current lines executing in this stack frame have been resolved.
+        /// </summary>
+        public bool Error { get; private set; }
+
+        /// <summary>
+        /// Indicates this stack frame has been resolved to code within a function definition.
+        /// </summary>
+        public bool ResolvedFunction
+        {
+            get
+            {
+                return Scope?.FunctionDefinition != null;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether the stack frame was mapped to a function, and whether the function is a constructor.
+        /// </summary>
+        public bool IsFunctionConstructor
+        {
+            get
+            {
+                return Scope?.FunctionDefinition?.IsConstructor == true;
+            }
+        }
+
+        /// <summary>
+        /// Indicates the name of the function. If this is a constructor without a name, it takes the name of its encapsulating contract.
+        /// </summary>
+        public string FunctionName
+        {
+            get
+            {
+                return IsFunctionConstructor ? Scope?.ContractDefinition?.Name : Scope?.FunctionDefinition?.Name;
+            }
+        }
+        #endregion
+
+        #region Fields
+        /// <summary>
+        /// The execution trace scope which defines the the scope this stack frame is currently executing in.
+        /// </summary>
+        public readonly ExecutionTraceScope Scope;
+
+        /// <summary>
+        /// The source files lines which indicate the current position in the stack frame.
+        /// </summary>
+        public readonly SourceFileLine[] CurrentPositionLines;
+
+        /// <summary>
+        /// Indicates a trace index which represents the current source position (<see cref="CurrentPositionLines"/>))
+        /// in this stack frame's scope.
+        /// </summary>
+        public readonly int CurrentPositionTraceIndex;
+        #endregion
+
+        #region Constructor
+        public ExecutionTraceStackFrame(ExecutionTraceScope scope, SourceFileLine[] currentLines, int lastTraceIndex, bool error)
+        {
+            // Set our properties
+            Scope = scope;
+            CurrentPositionLines = currentLines ?? Array.Empty<SourceFileLine>();
+            CurrentPositionTraceIndex = lastTraceIndex;
+            Error = error;
+        }
+        #endregion
+    }
+}
