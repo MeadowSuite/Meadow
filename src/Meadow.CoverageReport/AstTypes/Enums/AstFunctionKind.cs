d7381876de8e279b14d5795a4d01cb9dba71ab0d
David Pokora <dpokora@gmail.com>
2018-11-18 10:15:42 -0500

    Solidity 0.5.x: AstFunctionKind enum for "kind" token.

---
+++ src/Meadow.CoverageReport/AstTypes/Enums/AstFunctionKind.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meadow.CoverageReport.AstTypes.Enums
+{
+    public enum AstFunctionKind
+    {
+        Constructor,
+        Fallback,
+        Function
+    }
+}
